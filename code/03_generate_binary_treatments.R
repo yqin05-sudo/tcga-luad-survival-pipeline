bbf71c9dab2346fb20623b3e38deb9977b23d670
yqin05-sudo <yqin05@gmail.com>
2025-11-25 16:53:06 -0500

    Update 03_generate_binary_treatments.R

--- code/03_generate_binary_treatments.R
+++ code/03_generate_binary_treatments.R
@@ -1,1 +1,37 @@
+# 03_generate_binary_treatments.R
+# Deriving Surgery / Chemo / Radiotherapy binary flags
+
+library(tidyverse)
+library(stringr)
+
+# TODO:
+# - detect treatment_type patterns
+#   e.g., "surg", "chemo", "radiat"
+# - combine with treatment_or_therapy ("yes", "unknown")
+# - generate:
+#       has_surgery
+#       has_chemotherapy
+#       has_radiotherapy
+
+# Placeholder example:
+# treat_bin <- treat_raw %>%
+#   mutate(
+#     has_surgery = ifelse(
+#       str_detect(tolower(treatment_type), "surg") &
+#         treatment_or_therapy %in% c("yes", "unknown"),
+#       "Yes", "No"
+#     ),
+#     has_chemotherapy = ifelse(
+#       str_detect(tolower(treatment_type), "chemo") &
+#         treatment_or_therapy %in% c("yes", "unknown"),
+#       "Yes", "No"
+#     ),
+#     has_radiotherapy = ifelse(
+#       str_detect(tolower(treatment_type), "radiat") &
+#         treatment_or_therapy %in% c("yes", "unknown"),
+#       "Yes", "No"
+#     )
+#   )
+
+# saveRDS(treat_bin, "data_clean/luad_treatments.rds")
 
